4aae87a43872017b2407fd7121ceb21e5246b478
N8te <info@n8te.xyz>
2017-03-09 11:50:37 +0800

    Removed some data

--- experiments/getdata.php
+++ experiments/getdata.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error)
 } 
 
-$sql = "SELECT id FROM VIP";
+$sql = "SELECT id FROM blank";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
